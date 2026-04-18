693472064bbec91eeefe2da4c82c9d48d016db6e
kangnade <kangnade@gmail.com>
2026-04-18 00:02:16 -0700

    Added the ilinkmessage.go for defining the 3 types of interface for ilink messages

---
+++ sbe/ilinkmessage.go
@@ -0,0 +1,29 @@
+package sbe
+
+/*
+* Defines the ilink3 message interface
+* IlinkInbound, IlinkOutbound, and IlinkMessage
+* IlinkInbound interface: implements Decode, GetMessageTypeID, and PrettyPrint methods
+* InlinkOutbound interface: implements Encode, GetMessageTypeID, and PrettyPrint methods
+* InlinkMessage interface: implements Encode, Decode, GetMessageTypeID and PrettyPrint methods
+ */
+
+type IlinkOutbound interface{
+	Encode(c *Coder)
+	GetMessageTypeID() UInt16
+	PrettyPrint()
+}
+
+type IlinkInbound interface{
+	Decode(c *Coder)
+	GetMessageTypeID() UInt16
+	PrettyPrint()
+}
+
+type IlinkMessage interface{
+	Decode(c *Coder)
+	Encode(c *Coder)
+	GetMessageTypeID() UInt16
+	PrettyPrint()
+}
+
