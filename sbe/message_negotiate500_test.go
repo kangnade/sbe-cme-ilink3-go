3a80392d6898a92c2dca9ef5b064859d636d4803
kangnade <kangnade@gmail.com>
2026-06-08 15:29:15 -0700

    Added 5 test cases for Negotiate500 msg type

--- sbe/message_negotiate500_test.go
+++ sbe/message_negotiate500_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"sync"
+	"testing"
 )
 
 /*
@@ -69,4 +70,80 @@ func capturePrettyPrint(m *Negotiate500) string{
 	
 	// return the buf
 	return buf.String()
+}
+
+/*------- Tests -------*/
+
+func TestNewNegoatiate500_ConstantFields(t *testing.T){
+	// allows tests to be executed concurrently
+	t.Parallel()
+
+	m := NewNegotiate500()
+
+	if m.CustomerFlow != ClientFlowTypeValue{
+		t.Errorf("CustomerFlow = %v, expect = %v", m.CustomerFlow, ClientFlowTypeValue)
+	}
+
+	if m.HMACVersion != HMACVersionValue{
+		t.Errorf("HMACVersion = %v, expect = %v", m.HMACVersion, HMACVersionValue)
+	}
+}
+
+func TestNewNegotiate500_ZeroValueFields(t *testing.T){
+	t.Parallel()
+
+	m := NewNegotiate500()
+
+	if m.UUID != 0{
+		t.Errorf("UUID = %v, expect = 0", m.UUID)
+	}
+
+	if m.RequestTimestamp != 0{
+		t.Errorf("RequestTimestamp = %v, expect = 0", m.RequestTimestamp)
+	}
+
+  if m.Credentials.Length != 0{
+		t.Errorf("Credentials.Length = %v, expect = 0", m.Credentials.Length)
+	}
+}
+
+func TestGetMessageTypeID(t *testing.T){
+	t.Parallel()
+
+	m := NewNegotiate500()
+
+	if typeId := m.GetMessageTypeID(); typeId != 500{
+		t.Errorf("MessageTypeID = %v, expect = 500", typeId)
+	}
+}
+
+func TestBlockLength(t *testing.T){
+	t.Parallel()
+
+	m := NewNegotiate500()
+
+	if blockLen := m.BlockLength(); blockLen != 76{
+		t.Errorf("BlockLength = %v, expect = 76", blockLen)
+	}
+}
+
+func TestWireSize(t *testing.T){
+	t.Parallel()
+
+	tests := []struct{
+		name 			string
+		msg  			*Negotiate500
+		expected 	int
+	}{
+		{"No Credentials", makeNegotiate500(), 8 + 76 + 2 + 0},
+		{"With Credentials", makeNegotiate500WithCredentials(), 8 + 76 + 2 + 23},
+	}
+
+	for _, st := range tests{
+		t.Run(st.name, func(t *testing.T){
+			if size := st.msg.WireSize(); size != st.expected{
+				t.Errorf("WireSize() = %v, expected = %v", size, st.expected)
+			}
+		})
+	}
 }
